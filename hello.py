cf0f63f271e6cbcb520ac39b4e1dd0b356c53d71
abder <abder@Abder.localdomain>
2025-09-29 16:43:12 -0400

    ik heb de wijziging gemaakt op de 3de lijn

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
+
 hello Puthon
 
 1= "dream"
@@ -5,3 +6,6 @@ hello Puthon
 print 1
 print 2 
 
+klzaegla	k	mglr 
+aklzegrn lkj	lkj	rl rlk	 l	h l	krl	krhjkhrlhl 	
+	 	i hjrlhj	i	j rlhj		l rhj	rlhj			lt  
